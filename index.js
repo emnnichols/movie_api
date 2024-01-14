a01931f694341196113f157ee175e0d63b38988d
Emily N <emnnichols@outlook.com>
2024-01-13 18:32:13 -0800

    Created necessary arrays
    [Exercise 2.5] Variables for movies, directors, genres, users

--- index.js
+++ index.js
@@ -1,78 +1,308 @@
 const express = require('express'),
     morgan = require('morgan'),
     fs = require('fs'),
-    path = require('path');
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    uuid = require('uuid');
 
 const app = express();
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
 
-let topMovies = [
+const movies = [
     {
-        title: 'Howl\'s Moving Castle',
+        id: 1,
+        title: 'The Boy and the Heron',
         director: 'Hayao Miyazaki',
-        year: '2004'
+        year: 2023,
+        genre: 'Adventure'
     },
     {
-        title: 'Coraline',
-        director: 'Henry Selick',
-        year: '2009'
+        id: 2,
+        title: 'Castle in the Sky',
+        director: 'Hayao Miyazaki',
+        year: 1986,
+        genre: 'Adventure'
     },
     {
-        title: 'Eternal Sunshine of the Spotless Mind',
-        director: 'Michel Gondry',
-        year: '2004'
+        id: 3,
+        title: 'The Cat Returns',
+        director: 'Hiroyuki Morita',
+        year: 2002,
+        genre: 'Comedy'
     },
     {
-        title: '10 Things I Hate About You',
-        director: 'Gil Junger',
-        year: '1999'
+        id: 4,
+        title: 'Earwig and the Witch',
+        director: 'Gorô Miyazaki',
+        year: 2020,
+        genre: 'Family'
     },
     {
-        title: 'Addams Family Values',
-        director: 'Barry Sonnenfeld',
-        year: '1993'
+        id: 5,
+        title: 'From Up On Poppy Hill',
+        director: 'Gorô Miyazaki',
+        year: 2011,
+        genre: 'Drama'
     },
     {
-        title: 'Labyrinth',
-        director: 'Jim Henson',
-        year: '1986'
+        id: 6,
+        title: 'Grave of the Fireflies',
+        director: 'Isao Takahata',
+        year: 1988,
+        genre: 'War'
     },
     {
-        title: 'Practical Magic',
-        director: 'Griffin Dunne',
-        year: '1998'
+        id: 7,
+        title: 'Howl\s Moving Castle',
+        director: 'Hayao Miyazaki',
+        year: 2004,
+        genre: 'Adventure'
     },
     {
-        title: 'Moulin Rouge!',
-        director: 'Baz Luhrmann',
-        year: '2001'
+        id: 8,
+        title: 'Kiki\s Delivery Service',
+        director: 'Hayao Miyazaki',
+        year: 1989,
+        genre: 'Fantasy'
     },
     {
-        title: 'Dungeons & Dragons: Honor Among Thieves',
-        director: 'John Francis Daley and Jonathan Goldstein',
-        year: '2023'
+        id: 9,
+        title: 'My Neighbor Totoro',
+        director: 'Hayao Miyazaki',
+        year: 1988,
+        genre: 'Family'
     },
     {
-        title: 'Godzilla Minus One',
-        director: 'Takashi Yamazaki',
-        year: '2023'
+        id: 10,
+        title: 'My Neighbors The Yamadas',
+        director: 'Isao Takahata',
+        year: 1986,
+        genre: 'Family'
+    },
+    {
+        id: 11,
+        title: 'Nausicaä of the Valley of the Wind',
+        director: 'Hayao Miyazaki',
+        year: 1984,
+        genre: 'Sci-Fi'
+    },
+    {
+        id: 12,
+        title: 'Ocean Waves',
+        director: 'Tomomi Mochizuki',
+        year: 1993,
+        genre: 'Drama'
+    },
+    {
+        id: 13,
+        title: 'Only Yesterday',
+        director: 'Isao Takahata',
+        year: 1991,
+        genre: 'Romance'
+    },
+    {
+        id: 14,
+        title: 'Pom Poko',
+        director: 'Isao Takahata',
+        year: 1994,
+        genre: 'Comedy'
+    },
+    {
+        id: 15,
+        title: 'Ponyo',
+        director: 'Hayao Miyazaki',
+        year: 2008,
+        genre: 'Comedy'
+    },
+    {
+        id: 16,
+        title: 'Porco Rosso',
+        director: 'Hayao Miyazaki',
+        year: 1992,
+        genre: 'Comedy'
+    },
+    {
+        id: 17,
+        title: 'Princess Mononoke',
+        director: 'Hayao Miyazaki',
+        year: 1997,
+        genre: 'Action'
+    },
+    {
+        id: 18,
+        title: 'The Secret World of Arrietty',
+        director: 'Hiromasa Yonebayashi',
+        year: 2010,
+        genre: 'Adventure'
+    },
+    {
+        id: 19,
+        title: 'Spirited Away',
+        director: 'Hayao Miyazaki',
+        year: 2001,
+        genre: 'Adventure'
+    },
+    {
+        id: 20,
+        title: 'Tales from Earthsea',
+        director: 'Gorô Miyazaki',
+        year: 2006,
+        genre: 'Family'
+    },
+    {
+        id: 21,
+        title: 'The Tale of the Princess Kaguya',
+        director: 'Isao Takahata',
+        year: 2013,
+        genre: 'Drama'
+    },
+    {
+        id: 22,
+        title: 'When Marnie Was There',
+        director: 'Hiromasa Yonebayashi',
+        year: 2014,
+        genre: 'Family'
+    },
+    {
+        id: 23,
+        title: 'Whisper of the Heart',
+        director: 'Yoshifumi Kondô',
+        year: 1995,
+        genre: 'Drama'
+    },
+    {
+        id: 24,
+        title: 'The Wind Rises',
+        director: 'Hayao Miyazaki',
+        year: 2013,
+        genre: 'Biography'
+    }
+];
+
+const directors = [
+    {
+        id: 1,
+        name: 'Gorô Miyazaki',
+        birthday: 'Jan 21, 1967',
+        about: 'Gorô is the son of Hayao Miyazaki -- he is an animation director as well as a landscape architect. His landscape projects include the Ghibli Museum and Ghibli Park.'
+    },
+    {
+        id: 2,
+        name: 'Hayao Miyazaki',
+        birthday: 'Jan 5, 1941',
+        about: 'Hayao Miyazaki is a Japanese animator, filmmaker, and manga artist. He is a co-founder of Ghibli Studios and is widely regarded as one of the most accomplished filmmakers in animation.',
+    },
+    {
+        id: 3,
+        name: 'Hiromasa Yonebayashi',
+        birthday: 'July 10, 1973',
+        about: 'Hiromasa Yonebayashi is a Japanese animator and director, formerly for Studio Ghibli -- he left the studio after eighteen years in 2014. He established his own studio, Studio Ponoc, in June 2015.'
+    },
+    {
+        id: 4,
+        name: 'Hiroyuki Morita',
+        birthday: 'June 26, 1964',
+        about: 'Hiroyuki Morita is a Japanese animator and director, best known for working on the Studio Ghibli film The Cat Returns (2002). During high school, he produced an indepened animated film that won an animation magazine contest'
+    },
+    {
+        id: 5,
+        name: 'Isao Takahata',
+        birthday: 'Oct 29, 1935',
+        died: 'April 5, 2018',
+        about: 'Isao Takahata was a Japanese director, screenwriter, and producer. He was a co-founder of Studio Ghibli along side Miyazaki and others, where he went on to direct films such as Grave of the Fireflies, Pom Poko, and The Tale of the Princess Kaguya -- which was his last film as director'
+    },
+    {
+        id: 6,
+        name: 'Tomomi Mochizuki',
+        birthday: 'Dec 31, 1958',
+        about: 'Tomomi Mochizuki is a Japanese storyboard artist, screenwriter, and director. He is well known for working with animation studios such as Studio Ghibli, Nippon Animation, and Sunrise. He first gained recognition in the early 1990s after directing the show Ranma 1/2'
+    },
+    {
+        id: 7,
+        name: 'Yoshifumi Kondô',
+        birthday: 'March 31, 1950',
+        died: 'Jan 21, 1998',
+        about: 'Yoshifumi Kondô was a Japanese animator who worked for Studio Ghibli before his passing. His notable works include Kiki\s Delivery Service, Only Yesterday, and Princess Mononoke'
+    }
+];
+
+const genres = [
+    {
+        id: 1,
+        name: 'Action',
+        description: 'The protagonist is thrust into a series of events that typically involve violence and physical feats'
+    },
+    {
+        id: 2,
+        name: 'Adventure',
+        description: 'Defined by a journey and is usually located within a fantasy or exoticized setting'
+    },
+    {
+        id: 3,
+        name: 'Biography',
+        description: 'Dramatizes the life of a non-fictional or historically-based person or people'
+    },
+    {
+        id: 4,
+        name: 'Comedy',
+        description: 'Defined by events that are primarily intended to make the audience laugh'
+    },
+    {
+        id: 5,
+        name: 'Drama',
+        description: 'Focused on emotions and defined by conflict, often looking to reality'
+    },
+    {
+        id: 6,
+        name: 'Family',
+        description: 'Contains appropriate content for younger viewers but there may also be components of the film that are geared towards adults'
+    },
+    {
+        id: 7,
+        name: 'Fantasy',
+        description: 'Defined by situations that transcend natural laws and/or by settings inside a fictional universe'
+    },
+    {
+        id: 8,
+        name: 'Romance',
+        description: 'Characterized by an emphasis on passion, emotion, and the affectionate romantic involvement of the main characters'
+    },
+    {
+        id: 9,
+        name: 'Sci-Fi',
+        description: 'Defined by a combination of imaginative speculation and a scientific or technological premise'
+    },
+    {
+        id: 10,
+        name: 'War',
+        description: 'Concerned with warfare, with combat scenes and themes such as the effects of war on society and the moral and human issues raised by war'
+    }
+];
+
+let users = [
+    {
+        id: 1,
+        username: 'bettysgarden',
+        name: 'Betty',
+        email: '',
+        movies: []
+    },
+    {
+        id: 2,
+        username: 'meetmebehindthemall',
+        name: 'August',
+        email: '',
+        movies: []
     }
 ];
 
 app.use(morgan('combined', {stream: accessLogStream}));
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
-// GET requests
-app.get('/', (req, res) => {
-    res.send('Welcome to myFlix!');
-});
-
+// Get API documentation
 app.get('/documentation', (req, res) => {
     res.sendFile('public/documentation.html', { root: __dirname });
-});
-
-app.get('/movies', (req, res) => {
-    res.json(topMovies);
 });
 
 // error handling
