0bfcb35de98d25c3633d0e881cb798b7c712acc1
Emily N <emnnichols@outlook.com>
2024-01-27 16:11:29 -0800

    Updated database URI

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ const Users = Models.User;
 const app = express();
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
 
-mongoose.connect('mongodb://localhost:27017/myflixDB', { useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect('mongodb://localhost:27017/myflixDB', { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(morgan('combined', {stream: accessLogStream}));
 app.use(express.static('public'));
