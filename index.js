5d9e3f7715851c42991aef27e9d7c6b425d27c50
Emily N <emnnichols@outlook.com>
2024-02-22 10:43:31 -0800

    Update index.js

--- index.js
+++ index.js
@@ -205,10 +205,9 @@ app.put('/profile/:Username/account',
         return res.status(422).json({ errors: errors.array() });
     }
 
-    let currentPassword = req.user.Password;
     let hashedCurrent = Users.hashPassword(req.body.oldPassword);
 
-    if(hashedCurrent !== currentPassword){
+    if(hashedCurrent !== req.user.Password){
       return res.status(400).send('Incorrect password')
     }
 
