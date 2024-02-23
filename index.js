1f085050ce88e2afe4c5b0c0eb2fee485fdc6f3f
Emily N <emnnichols@outlook.com>
2024-02-23 12:14:46 -0800

    Added length min to password

--- index.js
+++ index.js
@@ -196,6 +196,7 @@ app.put('/profile/:Username/account',
     check('Username', 'Username is required').isLength({min: 5}),
     check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
     check('Password', 'Password is required').not().isEmpty(),
+    check('Password', 'Password is required').isLength({min: 8}),
     check('Email', 'Email does not appear to be valid').isEmail()
 ], passport.authenticate('jwt', { session: false }), async (req, res) => {
 
