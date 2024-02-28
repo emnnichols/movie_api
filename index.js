92f6cf24be8cac5636205ac2c0a2cb9c37b10e33
Emily N <emnnichols@outlook.com>
2024-02-27 16:35:20 -0800

    Changed director/about res

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ app.get('/movies/directors/:director', passport.authenticate('jwt', { session: f
 app.get('/movies/directors/:director/about', passport.authenticate('jwt', { session: false }), async (req, res) => {
     await Movies.findOne({ "Director.Name": req.params.director })
         .then((movie) => {
-            res.json(movie.Director);
+            res.json(movie.director);
         })
         .catch((err) => {
             console.error(err);
