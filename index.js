707a9d4e22bb0ef645c723531f20402afc96415f
Emily N <emnnichols@outlook.com>
2024-03-07 12:02:41 -0800

    Update CORS policy

--- index.js
+++ index.js
@@ -24,7 +24,18 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
 const cors = require('cors');
-app.use(cors());
+let allowedOrigins = ['http://localhost:1234', 'https://ghibli-flix.netlify.app'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 let auth = require('./auth')(app);
 const passport = require('passport');
